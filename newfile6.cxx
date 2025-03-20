2ec35b82d6ef417a5ffdd301e34ce0d8f4a1e634
oscarGR-123 <j.oscar.rf.223@gmail.com>
2025-03-20 08:37:07 -0600

    Add files via upload

---
+++ newfile6.cxx
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+    double a = 3.1;
+    double A = 4.5;
+    printf("%f", a+A);
+    printf("\n");
+    system("pause");
+    return 0;
+}
+    
